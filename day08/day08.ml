9e522dd3c3e0e9fe561be7395757d7922ffe9edc
Etienne Millon <me@emillon.org>
2023-12-08 14:37:59 +0100

    too slow

--- day08/day08.ml
+++ day08/day08.ml
@@ -17,8 +17,8 @@ let sample =
   |> String.concat_lines
 
 type move = L | R [@@deriving sexp]
-type node = { pos : string; left : string; right : string } [@@deriving sexp]
-type t = { moves : move list; nodes : node list } [@@deriving sexp]
+type node = { left : string; right : string } [@@deriving sexp]
+type t = { moves : move list; nodes : node Map.M(String).t } [@@deriving sexp]
 
 let parse s =
   let move =
@@ -31,18 +31,19 @@ let parse s =
   in
   let name =
     let open Angstrom in
-    take_while Char.is_alpha
+    take_while Char.is_alphanum
   in
   let node =
     let open Angstrom in
     let+ pos = name <* string " = ("
     and+ left = name <* string ", "
     and+ right = name <* string ")" <* end_of_line in
-    { pos; left; right }
+    (pos, { left; right })
   in
   let t =
     let open Angstrom in
-    let+ moves and+ nodes = many node in
+    let+ moves and+ node_list = many node in
+    let nodes = Map.of_alist_exn (module String) node_list in
     { moves; nodes }
   in
   Angstrom.parse_string ~consume:All t s |> Result.ok_or_failwith
@@ -53,24 +54,28 @@ let%expect_test "parse" =
     {|
     ((moves (R L))
      (nodes
-      (((pos AAA) (left BBB) (right CCC)) ((pos BBB) (left DDD) (right EEE))
-       ((pos CCC) (left ZZZ) (right GGG)) ((pos DDD) (left DDD) (right DDD))
-       ((pos EEE) (left EEE) (right EEE)) ((pos GGG) (left GGG) (right GGG))
-       ((pos ZZZ) (left ZZZ) (right ZZZ))))) |}]
+      ((AAA ((left BBB) (right CCC))) (BBB ((left DDD) (right EEE)))
+       (CCC ((left ZZZ) (right GGG))) (DDD ((left DDD) (right DDD)))
+       (EEE ((left EEE) (right EEE))) (GGG ((left GGG) (right GGG)))
+       (ZZZ ((left ZZZ) (right ZZZ)))))) |}]
 
-let result t =
+let result_gen t ~start_nodes ~end_nodes =
+  let is_end_node node = List.mem end_nodes node ~equal:String.equal in
   let rec go n cur = function
     | [] -> go n cur t.moves
     | move :: moves ->
-        if String.equal cur "ZZZ" then n
+        if List.for_all cur ~f:is_end_node then n
         else
-          let node =
-            List.find_exn t.nodes ~f:(fun { pos; _ } -> String.equal pos cur)
+          let next =
+            List.map cur ~f:(fun c ->
+                let { left; right } = Map.find_exn t.nodes c in
+                match move with L -> left | R -> right)
           in
-          let next = match move with L -> node.left | R -> node.right in
           go (n + 1) next moves
   in
-  go 0 "AAA" t.moves
+  go 0 start_nodes t.moves
+
+let result t = result_gen t ~start_nodes:[ "AAA" ] ~end_nodes:[ "ZZZ" ]
 
 let sample2 =
   [ "LLR"; ""; "AAA = (BBB, BBB)"; "BBB = (AAA, ZZZ)"; "ZZZ = (ZZZ, ZZZ)" ]
@@ -83,7 +88,34 @@ let%expect_test "result" =
   test sample2;
   [%expect {| 6 |}]
 
-let result2 _ = 0
+let sample_p2 =
+  [
+    "LR";
+    "";
+    "11A = (11B, XXX)";
+    "11B = (XXX, 11Z)";
+    "11Z = (11B, XXX)";
+    "22A = (22B, XXX)";
+    "22B = (22C, 22C)";
+    "22C = (22Z, 22Z)";
+    "22Z = (22B, 22B)";
+    "XXX = (XXX, XXX)";
+  ]
+  |> String.concat_lines
+
+let result2 t =
+  let all_nodes = Map.keys t.nodes in
+  let all_that_ends_with suffix =
+    List.filter_map all_nodes ~f:(fun pos ->
+        if String.is_suffix pos ~suffix then Some pos else None)
+  in
+  let start_nodes = all_that_ends_with "A" in
+  let end_nodes = all_that_ends_with "Z" in
+  result_gen t ~start_nodes ~end_nodes
+
+let%expect_test "result2" =
+  parse sample_p2 |> result2 |> printf "%d\n";
+  [%expect {| 6 |}]
 
 let run () =
   match Sys.get_argv () with
