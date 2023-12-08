f0527ee857e1da6db5beb6db3e6f8451ef86f681
Etienne Millon <me@emillon.org>
2023-12-08 10:31:36 +0100

    part 2

--- day06/day06.ml
+++ day06/day06.ml
@@ -43,20 +43,35 @@ let runs { time; distance } =
 let%expect_test "runs" =
   let test r = runs r |> [%sexp_of: (int * int * bool) list] |> print_s in
   test { time = 7; distance = 9 };
-  [%expect{|
+  [%expect
+    {|
     ((0 0 false) (1 6 false) (2 10 true) (3 12 true) (4 12 true) (5 10 true)
      (6 6 false) (7 0 false)) |}];
   test { time = 15; distance = 40 };
-  [%expect{|
+  [%expect
+    {|
     ((0 0 false) (1 14 false) (2 26 false) (3 36 false) (4 44 true) (5 50 true)
      (6 54 true) (7 56 true) (8 56 true) (9 54 true) (10 50 true) (11 44 true)
      (12 36 false) (13 26 false) (14 14 false) (15 0 false)) |}]
 
 let result_for_race r = runs r |> List.count ~f:(fun (_, _, ok) -> ok)
 let result l = List.map l ~f:result_for_race |> product
-let%expect_test "result" = parse sample |> result |> printf "%d\n";
+
+let%expect_test "result" =
+  parse sample |> result |> printf "%d\n";
   [%expect {| 288 |}]
-let result2 _ = 0
+
+let combine_inputs l =
+  let time_s, distance_s =
+    List.fold l ~init:("", "")
+      ~f:(fun (acc_times, acc_distances) { time; distance } ->
+        (acc_times ^ Int.to_string time, acc_distances ^ Int.to_string distance))
+  in
+  { time = Int.of_string time_s; distance = Int.of_string distance_s }
+
+let result2 l = combine_inputs l |> result_for_race
+let%expect_test "result" = parse sample |> result2 |> printf "%d\n";
+  [%expect {| 71503 |}]
 
 let run () =
   match Sys.get_argv () with
