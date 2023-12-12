5e409ac9d2a8dd7d0aea426bef2481b493a2c7c2
Etienne Millon <me@emillon.org>
2023-12-12 11:59:26 +0100

    part 2 (too slow)

--- day12/day12.ml
+++ day12/day12.ml
@@ -25,7 +25,14 @@ let condition_to_char = function
   | Operational -> '.'
   | Damaged -> '#'
 
-type line = { conditions : condition list; groups : int list } [@@deriving sexp]
+let conditions_to_string l =
+  List.map l ~f:condition_to_char |> String.of_char_list
+
+type conditions = condition list [@@deriving of_sexp]
+
+let sexp_of_conditions l = conditions_to_string l |> [%sexp_of: string]
+
+type line = { conditions : conditions; groups : int list } [@@deriving sexp]
 
 let conditions line = line.conditions
 
@@ -51,63 +58,46 @@ let%expect_test "parse" =
   parse sample |> [%sexp_of: t] |> print_s;
   [%expect
     {|
-    (((conditions (Unknown Unknown Unknown Operational Damaged Damaged Damaged))
-      (groups (1 1 3)))
-     ((conditions
-       (Operational Unknown Unknown Operational Operational Unknown Unknown
-        Operational Operational Operational Unknown Damaged Damaged Operational))
-      (groups (1 1 3)))
-     ((conditions
-       (Unknown Damaged Unknown Damaged Unknown Damaged Unknown Damaged Unknown
-        Damaged Unknown Damaged Unknown Damaged Unknown))
-      (groups (1 3 1 6)))
-     ((conditions
-       (Unknown Unknown Unknown Unknown Operational Damaged Operational
-        Operational Operational Damaged Operational Operational Operational))
-      (groups (4 1 1)))
-     ((conditions
-       (Unknown Unknown Unknown Unknown Operational Damaged Damaged Damaged
-        Damaged Damaged Damaged Operational Operational Damaged Damaged Damaged
-        Damaged Damaged Operational))
-      (groups (1 6 5)))
-     ((conditions
-       (Unknown Damaged Damaged Damaged Unknown Unknown Unknown Unknown Unknown
-        Unknown Unknown Unknown))
-      (groups (3 2 1)))) |}]
+    (((conditions ???.###) (groups (1 1 3)))
+     ((conditions .??..??...?##.) (groups (1 1 3)))
+     ((conditions ?#?#?#?#?#?#?#?) (groups (1 3 1 6)))
+     ((conditions ????.#...#...) (groups (4 1 1)))
+     ((conditions ????.######..#####.) (groups (1 6 5)))
+     ((conditions ?###????????) (groups (3 2 1)))) |}]
 
 let values_for = function
   | (Operational | Damaged) as c -> [ c ]
   | Unknown -> [ Operational; Damaged ]
 
-let rec values = function
-  | [] -> [ [] ]
+let rec iter_values ~f = function
+  | [] -> f []
   | c :: cs ->
-      let open List.Let_syntax in
       let hs = values_for c in
-      let%bind t = values cs in
-      let%map h = hs in
-      h :: t
+      iter_values cs ~f:(fun t -> List.iter hs ~f:(fun h -> f (h :: t)))
 
-let to_string l = List.map l ~f:condition_to_char |> String.of_char_list
+let values_for_test l =
+  let r = ref [] in
+  iter_values l ~f:(fun x -> r := x :: !r);
+  List.rev !r
 
 let%expect_test "values" =
-  parse sample |> List.hd_exn |> conditions |> values |> List.map ~f:to_string
-  |> List.iter ~f:(printf "%s\n");
+  parse sample |> List.hd_exn |> conditions |> values_for_test
+  |> [%sexp_of: conditions list] |> print_s;
   [%expect
     {|
-    ....###
-    #...###
-    .#..###
-    ##..###
-    ..#.###
-    #.#.###
-    .##.###
-    ###.### |}]
+    (....### #...### .#..### ##..### ..#.### #.#.### .##.### ###.###) |}]
 
 let groups l =
-  List.group l ~break:(fun a b -> not (equal_condition a b))
-  |> List.filter ~f:(fun group -> equal_condition (List.hd_exn group) Damaged)
-  |> List.map ~f:List.length
+  let state, r =
+    List.fold l ~init:(None, []) ~f:(fun (state, r) c ->
+        match (state, c) with
+        | _, Unknown -> assert false
+        | None, Operational -> (None, r)
+        | None, Damaged -> (Some 1, r)
+        | Some n, Damaged -> (Some (n + 1), r)
+        | Some n, Operational -> (None, n :: r))
+  in
+  match state with None -> List.rev r | Some n -> List.rev (n :: r)
 
 let%expect_test "groups" =
   let test s =
@@ -122,8 +112,10 @@ let%expect_test "groups" =
   [%expect "(2 3)"]
 
 let result_line { conditions; groups = expected_groups } =
-  values conditions
-  |> List.count ~f:(fun l -> [%equal: int list] (groups l) expected_groups)
+  let r = ref 0 in
+  iter_values conditions ~f:(fun l ->
+      if [%equal: int list] (groups l) expected_groups then Int.incr r);
+  !r
 
 let result l = List.map l ~f:result_line |> sum
 
@@ -131,11 +123,47 @@ let%expect_test "result" =
   parse sample |> result |> printf "%d\n";
   [%expect {| 21 |}]
 
-let result2 _ = 0
+let expand_conditions l =
+  List.concat
+    [ l; [ Unknown ]; l; [ Unknown ]; l; [ Unknown ]; l; [ Unknown ]; l ]
+
+let expand_groups l = List.concat [ l; l; l; l; l ]
+
+let expand_line { conditions; groups } =
+  { conditions = expand_conditions conditions; groups = expand_groups groups }
+
+let expand = List.map ~f:expand_line
+
+let%expect_test "expand" =
+  parse sample |> expand |> [%sexp_of: t] |> print_s;
+  [%expect
+    {|
+    (((conditions ???.###????.###????.###????.###????.###)
+      (groups (1 1 3 1 1 3 1 1 3 1 1 3 1 1 3)))
+     ((conditions
+       .??..??...?##.?.??..??...?##.?.??..??...?##.?.??..??...?##.?.??..??...?##.)
+      (groups (1 1 3 1 1 3 1 1 3 1 1 3 1 1 3)))
+     ((conditions
+       ?#?#?#?#?#?#?#???#?#?#?#?#?#?#???#?#?#?#?#?#?#???#?#?#?#?#?#?#???#?#?#?#?#?#?#?)
+      (groups (1 3 1 6 1 3 1 6 1 3 1 6 1 3 1 6 1 3 1 6)))
+     ((conditions
+       ????.#...#...?????.#...#...?????.#...#...?????.#...#...?????.#...#...)
+      (groups (4 1 1 4 1 1 4 1 1 4 1 1 4 1 1)))
+     ((conditions
+       ????.######..#####.?????.######..#####.?????.######..#####.?????.######..#####.?????.######..#####.)
+      (groups (1 6 5 1 6 5 1 6 5 1 6 5 1 6 5)))
+     ((conditions
+       ?###??????????###??????????###??????????###??????????###????????)
+      (groups (3 2 1 3 2 1 3 2 1 3 2 1 3 2 1)))) |}]
+
+let result2 t = expand t |> result
 
 let%expect_test "result2" =
   parse sample |> result2 |> printf "%d\n";
   [%expect {| 0 |}]
+
+(** XXX *)
+let result2 _ = 0
 
 let run () =
   match Sys.get_argv () with
