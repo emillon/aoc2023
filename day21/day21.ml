71cce83aa435d1184dc80e6686f5a1fbdab49e95
Etienne Millon <me@emillon.org>
2023-12-21 09:09:43 +0100

    p2 but slow

--- day21/day21.ml
+++ day21/day21.ml
@@ -72,23 +72,82 @@ let shift (i, j) = function
   | N -> (i, j - 1)
   | S -> (i, j + 1)
 
-let step m bounds start =
+let wrap_pos bounds (i, j) =
+  let { Map2d.imax; jmax; _ } = bounds in
+  (* XXX fix that at parse time *)
+  let imax = imax + 2 in
+  let jmax = jmax + 2 in
+  let i = i % imax in
+  let j = j % jmax in
+  (i, j)
+
+let mem_infinite m bounds p = Map2d.mem m (wrap_pos bounds p)
+
+let%expect_test "mem_infinite" =
+  let t = parse sample in
+  for j = -16 to 16 do
+    for i = -16 to 16 do
+      if mem_infinite t.map t.bounds (i, j) then printf "#" else printf "."
+    done;
+    printf "\n"
+  done;
+  [%expect
+    {|
+    ...#..##..#...#..##..#...#..##..#
+    .##.........##.........##........
+    ####..##.#.####..##.#.####..##.#.
+    #.##..##..##.##..##..##.##..##..#
+    .................................
+    .................................
+    ##.#......###.#......###.#......#
+    #..#..###.##..#..###.##..#..###.#
+    ..#....#.#...#....#.#...#....#.#.
+    #........#.#........#.#........#.
+    ####..##...####..##...####..##...
+    ...#..##..#...#..##..#...#..##..#
+    .##.........##.........##........
+    ####..##.#.####..##.#.####..##.#.
+    #.##..##..##.##..##..##.##..##..#
+    .................................
+    .................................
+    ##.#......###.#......###.#......#
+    #..#..###.##..#..###.##..#..###.#
+    ..#....#.#...#....#.#...#....#.#.
+    #........#.#........#.#........#.
+    ####..##...####..##...####..##...
+    ...#..##..#...#..##..#...#..##..#
+    .##.........##.........##........
+    ####..##.#.####..##.#.####..##.#.
+    #.##..##..##.##..##..##.##..##..#
+    .................................
+    .................................
+    ##.#......###.#......###.#......#
+    #..#..###.##..#..###.##..#..###.#
+    ..#....#.#...#....#.#...#....#.#.
+    #........#.#........#.#........#.
+    ####..##...####..##...####..##... |}]
+
+let blocked ~infinite m bounds p =
+  if infinite then mem_infinite m bounds p
+  else if Map2d.in_bounds bounds p then Map2d.mem m p
+  else true
+
+let step ~infinite m bounds start =
   Set.fold start
     ~init:(Set.empty (module Pos))
     ~f:(fun s pos ->
       all_dirs
       |> List.fold ~init:s ~f:(fun acc d ->
              let new_pos = shift pos d in
-             if Map2d.in_bounds bounds new_pos then
-               if Map2d.mem m new_pos then acc else Set.add acc new_pos
-             else acc))
+             if blocked ~infinite m bounds new_pos then acc
+             else Set.add acc new_pos))
 
 let%expect_test "step" =
   let t = parse sample in
   let start = Set.singleton (module Pos) t.start in
   let s = ref start in
   let go () =
-    s := step t.map t.bounds !s;
+    s := step ~infinite:false t.map t.bounds !s;
     Map2d.view ~sets:[ (!s, 'O'); (start, 'S') ] t.map (fun () -> "#")
   in
   go ();
@@ -133,9 +192,14 @@ let%expect_test "step" =
 
 let rec iterate_n x ~f ~n = if n = 0 then x else iterate_n (f x) ~f ~n:(n - 1)
 
-let result t =
-  iterate_n (Set.singleton (module Pos) t.start) ~f:(step t.map t.bounds) ~n:64
+let result_gen t ~n ~infinite =
+  iterate_n
+    (Set.singleton (module Pos) t.start)
+    ~f:(step ~infinite t.map t.bounds)
+    ~n
   |> Set.length
+
+let result t = result_gen t ~n:64 ~infinite:false
 
 let%expect_test "result" =
   parse sample |> result |> printf "%d\n";
@@ -143,8 +207,28 @@ let%expect_test "result" =
 
 let result2 _ = 0
 
-let%expect_test "result2" =
-  parse sample |> result2 |> printf "%d\n";
-  [%expect {| 0 |}]
+let%expect_test "result_gen" =
+  let t = parse sample in
+  let test n =
+    let result = result_gen ~n ~infinite:true t in
+    print_s [%message (n : int) (result : int)]
+  in
+  test 6;
+  [%expect {| ((n 6) (result 16)) |}];
+  test 10;
+  [%expect {| ((n 10) (result 50)) |}];
+  test 50;
+  [%expect {| ((n 50) (result 1594)) |}];
+  test 100;
+  [%expect {| ((n 100) (result 6536)) |}];
+  test 500;
+  [%expect {| ((n 500) (result 167004)) |}];
+  (*In exactly 1000 steps, he can reach 668697 garden plots.*)
+  (*test 1000;*)
+  (*[%expect {| 29 |}];*)
+  (*In exactly 5000 steps, he can reach 16733044 garden plots.*)
+  (*test 5000;*)
+  (*[%expect {| 29 |}]*)
+  ()
 
 let run () = main All parse result result2
