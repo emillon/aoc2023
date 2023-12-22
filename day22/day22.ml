a998c962f8c4056acd5420bd9c3ac3e56af11bf8
Etienne Millon <me@emillon.org>
2023-12-22 11:12:51 +0100

    part 2

--- day22/day22.ml
+++ day22/day22.ml
@@ -83,21 +83,30 @@ let is_oob s = Set.exists s ~f:(fun (_, _, z) -> z <= 0)
 
 let settle_step t =
   let set = build_set t in
-  List.map t ~f:(fun brick ->
+  List.fold_mapi t
+    ~init:(Set.empty (module Int))
+    ~f:(fun i acc brick ->
       let below = below brick in
-      if is_oob below then brick
-      else if intersects set below then brick
-      else move_down brick)
+      if is_oob below then (acc, brick)
+      else if intersects set below then (acc, brick)
+      else (Set.add acc i, move_down brick))
 
 (* XXX refactor to a sort of unfold *)
-let settle t = fixpoint t ~f:settle_step ~equal:[%equal: t]
+let settle t =
+  fixpoint
+    (t, Set.empty (module Int))
+    ~f:(fun (t, moved) ->
+      let moved_in_step, t' = settle_step t in
+      (t', Set.union moved moved_in_step))
+    ~equal:(fun (a, _) (b, _) -> [%equal: t] a b)
 
 let%expect_test "settle" =
-  parse sample |> settle |> [%sexp_of: t] |> print_s;
+  parse sample |> settle |> [%sexp_of: t * Set.M(Int).t] |> print_s;
   [%expect
     {|
-    (((1 0 1) (1 2 1)) ((0 0 2) (2 0 2)) ((0 2 2) (2 2 2)) ((0 0 3) (0 2 3))
-     ((2 0 3) (2 2 3)) ((0 1 4) (2 1 4)) ((1 1 5) (1 1 6))) |}]
+    ((((1 0 1) (1 2 1)) ((0 0 2) (2 0 2)) ((0 2 2) (2 2 2)) ((0 0 3) (0 2 3))
+      ((2 0 3) (2 2 3)) ((0 1 4) (2 1 4)) ((1 1 5) (1 1 6)))
+     (2 3 4 5 6)) |}]
 
 let rec removed = function
   | [] -> []
@@ -108,20 +117,22 @@ let%expect_test "removed" =
   [%expect {| ((2 3 4) (1 3 4) (1 2 4) (1 2 3)) |}]
 
 let result t =
-  let t = settle t in
-  settle t |> removed
+  settle t |> fst |> removed
   |> List.count ~f:(fun l ->
-         let l' = settle_step l in
+         let l' = settle_step l |> snd in
          [%equal: t] l l')
 
 let%expect_test "result" =
   parse sample |> result |> printf "%d\n";
   [%expect {| 5 |}]
 
-let result2 _ = 0
+let result2 t =
+  settle t |> fst |> removed
+  |> List.map ~f:(fun l -> settle l |> snd |> Set.length)
+  |> sum
 
 let%expect_test "result2" =
   parse sample |> result2 |> printf "%d\n";
-  [%expect {| 0 |}]
+  [%expect {| 7 |}]
 
 let run () = main All parse result result2
